43897aa5f3d2697ab78e0b1c826687709ddfdd43
ArturGalstyan96 <artur868artur868.1996@gmail.com>
2023-05-04 00:18:03 +0400

    initial commit

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def getQrCode(url='https://www.google.com/', name='default'):
     return f'(QR code was created successfully, Open the{name}.png)'
 
 def main():
-    print(getQrCode(url='https://github.com/ArturPy88', name='Github'))
-    # print(getQrCode(url='https://www.linkedin.com/in/artur-galstyan-77bb84164/', name='Linkedin'))
+    print(getQrCode(url='https://github.com/ArturGalstyan96', name='Github'))
+    print(getQrCode(url='https://www.linkedin.com/in/artur-galstyan-77bb84164/', name='Linkedin'))
+    print(getQrCode(url='https://www.codewars.com/users/ArturPy88', name='Codewars'))
+
+
 
 
 if __name__ == '__main__':
